66c49155f8890a9fa5c4c51c607999e6c9858f89
Saanu Reghunadh <saanu09xs@gmail.com>
2020-09-14 14:25:37 +0530

    added empty vc remove service

--- src/service.rs
+++ src/service.rs
@@ -1,4 +1,4 @@
-use crate::{get_all_guilds, MongoClient};
+use crate::{get_all_guilds, MongoClient, PlayerManager, VoiceManager};
 use itconfig::*;
 use rand::seq::SliceRandom;
 use serenity::{
@@ -7,7 +7,7 @@ use serenity::{
 };
 use std::collections::HashSet;
 use std::{sync::Arc, time::Duration};
-use tracing::debug;
+use tracing::{debug, info};
 use Vec;
 
 async fn hydrate_reminder(ctx: Arc<Context>) {
@@ -49,9 +49,44 @@ async fn status_update(ctx: Arc<Context>) {
     debug!("Status update done.");
 }
 
+async fn check_vc_empty(ctx: Arc<Context>) {
+    let data = ctx.data.read().await;
+    let manager_lock = data
+        .get::<VoiceManager>()
+        .cloned()
+        .expect("Expected VoiceManager in ShareMap.");
+    let player_lock = data
+        .get::<PlayerManager>()
+        .cloned()
+        .expect("Expected Player Manger in TypeMap");
+    for guild_id in &ctx.cache.guilds().await {
+        let mut manager = manager_lock.lock().await;
+        if let Some(handler) = manager.get_mut(guild_id) {
+            let guild = ctx.cache.guild(guild_id).await.unwrap();
+            if let Some(channel) = guild.channels.get(&handler.channel_id.unwrap()) {
+                if let Ok(members) = channel.members(&ctx).await {
+                    if members.len() == 1 {
+                        let mut pm = player_lock.write().await;
+                        if let Some(player) = pm.get_mut(&(guild_id.0 as u64)) {
+                            if !player.is_finished().await {
+                                player.reset();
+                                handler.stop();
+                            }
+                            pm.remove(&(guild_id.0 as u64));
+                        }
+                        manager.remove(guild_id);
+                        debug!("Removed vc cause of inactivity to guild: {:?}", guild_id);
+                    }
+                }
+            }
+        }
+    }
+}
+
 pub async fn service_loop(ctx: Arc<Context>) {
     let ctx_clone1 = Arc::clone(&ctx);
     let ctx_clone2 = Arc::clone(&ctx);
+    let ctx_clone3 = Arc::clone(&ctx);
     tokio::spawn(async move {
         loop {
             tokio::join!(hydrate_reminder(Arc::clone(&ctx_clone1)));
@@ -62,6 +97,12 @@ pub async fn service_loop(ctx: Arc<Context>) {
         loop {
             tokio::join!(status_update(Arc::clone(&ctx_clone2)));
             tokio::time::delay_for(Duration::from_secs(5)).await;
+        }
+    });
+    tokio::spawn(async move {
+        loop {
+            tokio::join!(check_vc_empty(Arc::clone(&ctx_clone3)));
+            tokio::time::delay_for(Duration::from_secs(900)).await;
         }
     });
 }
