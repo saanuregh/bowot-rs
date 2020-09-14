7e409387fd8eb779bcd842a59e53bb6a51779431
Saanu Reghunadh <saanu09xs@gmail.com>
2020-09-14 20:13:52 +0530

    purge command will auto delete the success message

--- src/commands/moderation.rs
+++ src/commands/moderation.rs
@@ -197,9 +197,12 @@ async fn clear(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
             channel.delete_messages(ctx, messages_ids).await?;
 
-            msg.channel_id
-                .say(ctx, format!("Successfully deleted `{}` message", n))
+            let success_msg = msg
+                .channel_id
+                .say(ctx, format!("Successfully deleted `{}` message, This message will self-delete in 5 seconds", n))
                 .await?;
+            tokio::time::delay_for(std::time::Duration::from_secs(5)).await;
+            success_msg.delete(ctx).await?;
         }
     }
     Ok(())
