e4a1da087d6ab819d1e363fc35c6aa4f71d602ea
Saanu Reghunadh <saanu09xs@gmail.com>
2020-12-16 21:29:40 +0530

    refactored some stuff

--- src/commands/meta.rs
+++ src/commands/meta.rs
@@ -1,21 +1,11 @@
-use crate::{ShardManagerContainer, Uptime};
-use num_format::{Locale, ToFormattedString};
-use serde_json::json;
+use crate::utils::basic_functions::{
+    get_rest_latency, get_shard_latency, meta_info, MetaInfoResult,
+};
 use serenity::{
-    client::bridge::gateway::ShardId,
     framework::standard::{macros::command, CommandResult},
     model::{channel::Message, Permissions},
     prelude::Context,
 };
-use std::{
-    fs::{read_to_string, File},
-    io::prelude::*,
-    process::id,
-    time::Instant,
-};
-use tokio::process::Command;
-use toml::Value;
-use walkdir::WalkDir;
 
 /// This command just sends an invite of the bot with the required permissions.
 #[command]
@@ -71,7 +61,7 @@ async fn invite(ctx: &Context, msg: &Message) -> CommandResult {
                 ("Add Reactions", "To be able to add reactions for all the paginated commands.", true),
                 ("Speak", "To be able to play music on that voice channel.", true),
                 ("Connect", "To be able to connect to a voice channel.", true),
-       ]);
+            ]);
             e
         });
 
@@ -84,38 +74,17 @@ async fn invite(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 #[aliases("pong", "latency")]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
-    let data = ctx.data.read().await;
-    let shard_manager = match data.get::<ShardManagerContainer>() {
-        Some(v) => v,
-        None => {
-            msg.reply(ctx, "There was a problem getting the shard manager")
-                .await?;
-            return Ok(());
-        }
-    };
-    let manager = shard_manager.lock().await;
-    let runners = manager.runners.lock().await;
-    let runner = match runners.get(&ShardId(ctx.shard_id)) {
-        Some(runner) => runner,
-        None => {
-            msg.reply(ctx, "No shard found").await?;
-            return Ok(());
-        }
-    };
-    let shard_latency = match runner.latency {
-        Some(ms) => format!("{:.2}ms", ms.as_micros() as f32 / 1000.0),
-        _ => String::new(),
-    };
-    let map = json!({"content" : "Calculating latency..."});
-    let now = Instant::now();
-    let mut message = ctx.http.send_message(msg.channel_id.0, &map).await?;
-    let rest_latency = now.elapsed().as_millis();
+    let shard_latency = get_shard_latency(ctx).await;
+    let (rest_latency, mut message) = get_rest_latency(ctx, msg.channel_id.0).await?;
     message
         .edit(ctx, |m| {
-            m.content(format!(
-                "Ping?\nGateway: {}\nREST: {}ms",
-                shard_latency, rest_latency
-            ))
+            m.content("");
+            m.embed(|e| {
+                e.title("Ping").fields(vec![
+                    ("Gateway", shard_latency + "ms", false),
+                    ("REST", rest_latency.to_string() + "ms", false),
+                ])
+            })
         })
         .await?;
     Ok(())
@@ -125,97 +94,27 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 #[command]
 #[aliases(info)]
 async fn about(ctx: &Context, msg: &Message) -> CommandResult {
-    let shard_latency = {
-        let data = ctx.data.read().await;
-        let shard_manager = data.get::<ShardManagerContainer>().unwrap();
-
-        let manager = shard_manager.lock().await;
-        let runners = manager.runners.lock().await;
-
-        let runner_raw = runners.get(&ShardId(ctx.shard_id));
-        if let Some(runner) = runner_raw {
-            match runner.latency {
-                Some(ms) => format!("{}ms", ms.as_millis()),
-                _ => "?ms".to_string(),
-            }
-        } else {
-            "?ms".to_string()
-        }
-    };
-    let uptime = {
-        let data = ctx.data.read().await;
-        let instant = data.get::<Uptime>().unwrap();
-        let duration = instant.elapsed();
-        humantime::format_duration(duration)
-    };
-    let map = json!({"content" : "Calculating latency..."});
-    let now = Instant::now();
-    let mut message = ctx.http.send_message(msg.channel_id.0, &map).await?;
-    let rest_latency = now.elapsed().as_millis();
-    let pid = id().to_string();
-    let full_stdout = Command::new("sh")
-        .arg("-c")
-        .arg(format!(r"pmap {} | tail -n 1 | awk '/[0-9]K/{{print $2}}'", &pid).as_str())
-        .output()
-        .await
-        .expect("failed to execute process");
-    let reasonable_stdout = Command::new("sh")
-        .arg("-c")
-        .arg(
-            format!(
-                "pmap {} | head -n 2 | tail -n 1 | awk '/[0-9]K/{{print $2}}'",
-                &pid
-            )
-            .as_str(),
-        )
-        .output()
-        .await
-        .expect("failed to execute process");
-
-    let mut full_mem = String::from_utf8(full_stdout.stdout).unwrap();
-    let mut reasonable_mem = String::from_utf8(reasonable_stdout.stdout).unwrap();
-    full_mem.pop();
-    full_mem.pop();
-    reasonable_mem.pop();
-    reasonable_mem.pop();
-    let version = {
-        let mut file = File::open("Cargo.toml")?;
-        let mut contents = String::new();
-        file.read_to_string(&mut contents)?;
-
-        let data = contents.parse::<Value>().unwrap();
-        let version = data["package"]["version"].as_str().unwrap();
-        version.to_string()
-    };
-    let (hoster_tag, hoster_id) = {
-        let app_info = ctx.http.get_current_application_info().await?;
-        (app_info.owner.tag(), app_info.owner.id)
-    };
-    let current_user = ctx.cache.current_user().await;
-    let bot_name = &current_user.name;
-    let bot_icon = &current_user.avatar_url();
-    let num_guilds = ctx.cache.guilds().await.len();
-    let num_shards = ctx.cache.shard_count().await;
-    let num_channels = ctx.cache.guild_channel_count().await;
-    let num_priv_channels = ctx.cache.private_channels().await.len();
-    let mut c_blank = 0;
-    let mut c_comment = 0;
-    let mut c_code = 0;
-    let mut c_lines = 0;
-    let mut command_count = 0;
-    for entry in WalkDir::new("src") {
-        let entry = entry.unwrap();
-        let path = entry.path();
-        if path.is_file() {
-            let count = loc::count(path.to_str().unwrap());
-            let text = read_to_string(&path)?;
-            command_count += text.match_indices("#[command]").count();
-            c_blank += count.blank;
-            c_comment += count.comment;
-            c_code += count.code;
-            c_lines += count.lines;
-        }
-    }
+    let (rest_latency, mut message) = get_rest_latency(ctx, msg.channel_id.0).await?;
+    let MetaInfoResult {
+        shard_latency,
+        uptime,
+        full_mem,
+        reasonable_mem,
+        version,
+        c_blank,
+        c_comment,
+        c_code,
+        c_lines,
+        command_count,
+        hoster_tag,
+        hoster_id,
+        bot_name,
+        bot_icon,
+        num_guilds,
+        num_shards,
+        num_channels,
+        num_priv_channels,
+    } = meta_info(ctx).await;
     message.edit(ctx, |m| {
         m.content("");
         m.embed(|e| {
@@ -225,11 +124,8 @@ async fn about(ctx: &Context, msg: &Message) -> CommandResult {
             e.field("Statistics:", format!("Shards: {}\nGuilds: {}\nChannels: {}\nPrivate Channels: {}", num_shards, num_guilds, num_channels, num_priv_channels), true);
             e.field("Lines of code:", format!("Blank: {}\nComment: {}\nCode: {}\nTotal Lines: {}", c_blank, c_comment, c_code, c_lines), true);
             e.field("Currently hosted by:", format!("Tag: {}\nID: {}", hoster_tag, hoster_id), true);
-            e.field("Latency:", format!("Gateway:\n`{}`\nREST:\n`{}ms`", shard_latency, rest_latency), true);
-            e.field("Memory usage:", format!("Complete:\n`{} KB`\nBase:\n`{} KB`",
-                                            &full_mem.parse::<u32>().expect("NaN").to_formatted_string(&Locale::en),
-                                            &reasonable_mem.parse::<u32>().expect("NaN").to_formatted_string(&Locale::en)
-                                            ), true);
+            e.field("Latency:", format!("Gateway:\n`{}ms`\nREST:\n`{}ms`", shard_latency, rest_latency), true);
+            e.field("Memory usage:", format!("Complete:\n`{} KB`\nBase:\n`{} KB`",full_mem, reasonable_mem), true);
             e.field("Somewhat Static Stats:", format!("Command Count:\n`{}`\nUptime:\n`{}`", command_count, uptime), true);
             if let Some(x) = bot_icon {
                 e.thumbnail(x);
--- src/utils/basic_functions.rs
+++ src/utils/basic_functions.rs
@@ -1,3 +1,14 @@
+use crate::{ShardManagerContainer, Uptime};
+use lazy_static::lazy_static;
+use num_format::{Locale, ToFormattedString};
+use serde::Serialize;
+use serde_json::json;
+use serenity::{client::bridge::gateway::ShardId, model::channel::Message, prelude::Context};
+use std::{fs::read_to_string, process::id};
+use tokio::{process::Command, time::Instant};
+use toml::Value;
+use walkdir::WalkDir;
+
 // Capitalizes the first letter of a str.
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
@@ -51,4 +62,163 @@ pub fn string_to_seconds(text: impl ToString) -> u64 {
     }
 
     seconds
+}
+
+#[derive(Default)]
+struct CodeStruct {
+    c_blank: u32,
+    c_comment: u32,
+    c_code: u32,
+    c_lines: u32,
+    command_count: u32,
+}
+
+lazy_static! {
+    static ref CODE: CodeStruct = {
+        let mut c = CodeStruct::default();
+        for entry in WalkDir::new("src") {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_file() {
+                let count = loc::count(path.to_str().unwrap());
+                let text = read_to_string(&path).unwrap();
+                c.command_count += text.match_indices("#[command]").count() as u32;
+                c.c_blank += count.blank;
+                c.c_comment += count.comment;
+                c.c_code += count.code;
+                c.c_lines += count.lines;
+            }
+        }
+        c
+    };
+    static ref VERSION: String = {
+        let data = include_str!("../../Cargo.toml").parse::<Value>().unwrap();
+        let version = data["package"]["version"].as_str().unwrap();
+        version.to_string()
+    };
+}
+
+#[derive(Serialize)]
+pub struct MetaInfoResult {
+    pub shard_latency: String,
+    pub uptime: String,
+    pub full_mem: String,
+    pub reasonable_mem: String,
+    pub version: String,
+    pub c_blank: u32,
+    pub c_comment: u32,
+    pub c_code: u32,
+    pub c_lines: u32,
+    pub command_count: u32,
+    pub hoster_tag: String,
+    pub hoster_id: u64,
+    pub bot_name: String,
+    pub bot_icon: Option<String>,
+    pub num_guilds: usize,
+    pub num_shards: u64,
+    pub num_channels: usize,
+    pub num_priv_channels: usize,
+}
+
+pub async fn get_rest_latency(ctx: &Context, channel_id: u64) -> serenity::Result<(u128, Message)> {
+    let map = json!({"content" : "Calculating latency..."});
+    let now = Instant::now();
+    let message = ctx.http.send_message(channel_id, &map).await?;
+    let rest_latency = now.elapsed().as_millis();
+    Ok((rest_latency, message))
+}
+
+pub async fn get_shard_latency(ctx: &Context) -> String {
+    let data = ctx.data.read().await;
+    let shard_manager = data.get::<ShardManagerContainer>().unwrap();
+    let manager = shard_manager.lock().await;
+    let runners = manager.runners.lock().await;
+    let runner_raw = runners.get(&ShardId(ctx.shard_id));
+    if let Some(runner) = runner_raw {
+        if let Some(ms) = runner.latency {
+            return format!("{}", ms.as_millis());
+        }
+    }
+    "?".to_string()
+}
+
+pub async fn get_uptime(ctx: &Context) -> String {
+    let data = ctx.data.read().await;
+    let instant = data.get::<Uptime>().unwrap();
+    let duration = instant.elapsed();
+    humantime::format_duration(duration).to_string()
+}
+
+pub async fn get_memory_usage() -> (String, String) {
+    let pid = id().to_string();
+    let full_stdout = Command::new("sh")
+        .arg("-c")
+        .arg(format!(r"pmap {} | tail -n 1 | awk '/[0-9]K/{{print $2}}'", &pid).as_str())
+        .output()
+        .await
+        .expect("failed to execute process");
+    let reasonable_stdout = Command::new("sh")
+        .arg("-c")
+        .arg(
+            format!(
+                "pmap {} | head -n 2 | tail -n 1 | awk '/[0-9]K/{{print $2}}'",
+                &pid
+            )
+            .as_str(),
+        )
+        .output()
+        .await
+        .expect("failed to execute process");
+    let mut full_mem = String::from_utf8(full_stdout.stdout).unwrap();
+    let mut reasonable_mem = String::from_utf8(reasonable_stdout.stdout).unwrap();
+    full_mem.pop();
+    full_mem.pop();
+    full_mem = full_mem
+        .parse::<u32>()
+        .expect("NaN")
+        .to_formatted_string(&Locale::en);
+    reasonable_mem.pop();
+    reasonable_mem.pop();
+    reasonable_mem = reasonable_mem
+        .parse::<u32>()
+        .expect("NaN")
+        .to_formatted_string(&Locale::en);
+    (full_mem, reasonable_mem)
+}
+
+pub async fn meta_info(ctx: &Context) -> MetaInfoResult {
+    let shard_latency = get_shard_latency(ctx).await;
+    let uptime = get_uptime(ctx).await;
+    let (hoster_tag, hoster_id) = {
+        let app_info = ctx.http.get_current_application_info().await.unwrap();
+        (app_info.owner.tag(), app_info.owner.id.as_u64().clone())
+    };
+    let (full_mem, reasonable_mem) = get_memory_usage().await;
+    let current_user = ctx.cache.current_user().await;
+    let bot_name = current_user.name.clone();
+    let bot_icon = current_user.avatar_url();
+    let num_guilds = ctx.cache.guilds().await.len();
+    let num_shards = ctx.cache.shard_count().await;
+    let num_channels = ctx.cache.guild_channel_count().await;
+    let num_priv_channels = ctx.cache.private_channels().await.len();
+    MetaInfoResult {
+        shard_latency,
+        bot_icon,
+        bot_name,
+        full_mem,
+        reasonable_mem,
+        uptime,
+        version: VERSION.clone(),
+        hoster_id,
+        hoster_tag,
+        num_channels,
+        num_guilds,
+        num_priv_channels,
+        num_shards,
+        c_blank: CODE.c_blank,
+        c_code: CODE.c_code,
+        c_comment: CODE.c_comment,
+        c_lines: CODE.c_lines,
+        command_count: CODE.command_count,
+    }
 }
